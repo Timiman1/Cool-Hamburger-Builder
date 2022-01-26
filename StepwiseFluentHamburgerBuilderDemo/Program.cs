6da6fea92336443f7cce7d95bbf12d13ba9f1e0e
Timiman1 <tim.toresson@iths.se>
2022-01-26 19:14:43 +0100

    Added console input logic and UI, so now the user can build his own favourite hamburger step-by-step via the console UI.

--- StepwiseFluentHamburgerBuilderDemo/Program.cs
+++ StepwiseFluentHamburgerBuilderDemo/Program.cs
@@ -6,24 +6,59 @@ namespace StepwiseFluentHamburgerBuilderDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***Builder Pattern Demo***");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("***Build a Hamburger!***\n");
+
             var burger = HamburgerBuilder.Create().
-                OfType(HamburgerType.Vegan).
-                WithBun(ServingSize.Medium).
-                WithLettuce(ServingSize.Medium).
-                WithCheese(ServingSize.Extra).
-                WithOnion(ServingSize.Extra).
-                WithPatty(ServingSize.Medium).
-                WithKetchup(ServingSize.None).
-                WithCucumber(ServingSize.Medium).
-                WithTomato(ServingSize.Extra).
+                OfType(GetHamburgerTypeFromUserSelection()).
+                WithBun(GetServingSizeFromUserSelectionOfItem("bun")).
+                WithLettuce(GetServingSizeFromUserSelectionOfItem("lettuce")).
+                WithCheese(GetServingSizeFromUserSelectionOfItem("cheese")).
+                WithOnion(GetServingSizeFromUserSelectionOfItem("onion")).
+                WithPatty(GetServingSizeFromUserSelectionOfItem("patty")).
+                WithKetchup(GetServingSizeFromUserSelectionOfItem("ketchup")).
+                WithCucumber(GetServingSizeFromUserSelectionOfItem("cucumber")).
+                WithTomato(GetServingSizeFromUserSelectionOfItem("tomato")).
                 Build();
 
-            Console.WriteLine("Hamburger was assembled!");
+            Console.WriteLine("Your Hamburger is finished and ready to serve!");
             Console.WriteLine(burger.ToString());
-            Console.WriteLine("The ingredients are hardcoded, change the serving sizes in Program.cs to style your own awesome burger :)");
-
             Console.ReadKey();
+        }
+
+        private static HamburgerType GetHamburgerTypeFromUserSelection()
+        {
+            Console.WriteLine("Select a hamburger type:");
+            return (HamburgerType)GetUserSelectionOfType(typeof(HamburgerType));
+
+        }
+
+        private static ServingSize GetServingSizeFromUserSelectionOfItem(string itemName)
+        {
+            Console.WriteLine($"Select serving size of {itemName}:");
+            return (ServingSize)GetUserSelectionOfType(typeof(ServingSize));
+        }
+
+        private static int GetUserSelectionOfType(Type type)
+        {
+            foreach (int i in Enum.GetValues(type))
+            {
+                string name = Enum.GetName(type, i);
+                Console.WriteLine($"{i}. {name}");
+            }
+            int input;
+            while (true)
+            {
+                input = Console.ReadKey().KeyChar - '0';
+                if (input >= 0 && input <= 2)
+                    break;
+                Console.CursorLeft = 0;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Bad input");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("\n");
+            return input;
         }
     }
 }
